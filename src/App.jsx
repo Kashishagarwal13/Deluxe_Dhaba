28062c5a3e417dd6f67156e40b994af7e6939788
Kashishagarwal13 <kashishagarwal1325@gmail.com>
2024-04-08 22:07:32 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Layout from './Pages/Layout';
@@ -7,20 +7,34 @@ import Menu from './Pages/Menu';
 import Contact from './Pages/Contact';
 import viteLogo from '/vite.svg'
 import './App.css'
+import Preloader from './components/Preloader';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay with setTimeout
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the delay time as needed
+  }, []);
 
 
-function App() {
   return (
-
-    <Routes>
+    <div className="app">
+      {isLoading ? <Preloader /> : (  <Routes>
       <Route path="/" element={<Layout />}> {/* Wrap all routes with Layout */}
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path='menu' element={<Menu />} />
         <Route path='contactus' element={<Contact />} />
       </Route>
-    </Routes>
-  )
-}
+    </Routes>)}
+    </div>
 
-export default App
+  
+  );
+      };
+
+
+export default App;
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Preloader.css'; // Import CSS file for styling
+import logo from '../assets/images/LOGO_Finalpng.png'
+
+const Preloader = () => {
+  return (
+    <div className="preloader flex flex-col">
+    <p className=" text1" >Welcome To Delux Dhaba</p>
+   <img src={logo} alt="Logo" className="logo mt-2" />
+    </div>
+  );
+};
+
+export default Preloader;
